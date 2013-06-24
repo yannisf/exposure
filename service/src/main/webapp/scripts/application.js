3e65afb648e9053192067954332d769a53d5a4a2
yannisf <yannisf@tragana>
2013-06-24 12:06:01 +0300

    Added angular services

--- service/src/main/webapp/scripts/application.js
+++ service/src/main/webapp/scripts/application.js
@@ -1,31 +1,57 @@
 var exposureModule = angular.module('exposure', []);
 
-exposureModule.config( function($httpProvider) {
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+exposureModule.factory('Iso', function($http) {
+    var isos = {};
+    isos.query = function() {
+        return $http.get("api/iso").then(function (response) {
+                return response.data;
+            }
+        );
+    }
+
+    return isos;
+});
+
+exposureModule.factory('Aperture', function($http) {
+    var apertures = {};
+    apertures.query = function() {
+        return $http.get("api/aperture").then(function (response) {
+                return response.data;
+            }
+        );
+    }
+
+    return apertures;
+});
+
+exposureModule.factory('Shutter', function($http) {
+    var shutters = {};
+    shutters.query = function() {
+        return $http.get("api/shutter").then(function (response) {
+                return response.data;
+            }
+        );
+    }
+
+    return shutters;
 });
 
 exposureModule.controller('InitializeController',
 
-    function($scope, $http) {
-        $http.get("api/iso").success(
-            function (data, status, headers, config) {
-                $scope.isos = data;
-                $scope.isoSet = data[0];
-            }
-        );
+    function($scope, Iso, Aperture, Shutter) {
+        Iso.query().then(function(data) {
+            $scope.isos = data;
+            $scope.isoSet = $scope.isos[0];
+        });
 
-        $http.get("api/aperture").success(
-            function (data, status, headers, config) {
-                $scope.apertures = data;
-                $scope.apertureSet = data[0];
-            }
-        );
+        Aperture.query().then(function(data) {
+            $scope.apertures = data;
+            $scope.apertureSet = $scope.apertures[0];
+        });
 
-        $http.get("api/shutter").success(
-            function (data, status, headers, config) {
-                $scope.shutters = data;
-                $scope.shutterSet = data[0];
-            }
-        );
+        Shutter.query().then(function(data) {
+            $scope.shutters = data;
+            $scope.shutterSet = $scope.shutters[0];
+        });
 
 });
